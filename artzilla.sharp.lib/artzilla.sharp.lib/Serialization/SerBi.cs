3db05eca7dd6e6a0a490de185fa4daac3fde19b3
Artyom Fomin <mail@artzilla.name>
2016-03-03 22:01:57 +0600

    Added serialization help class

---
+++ artzilla.sharp.lib/artzilla.sharp.lib/Serialization/SerBi.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using ArtZilla.Sharp.Lib.Extenstions;
+
+namespace ArtZilla.Sharp.Lib.Serialization {
+	public class SerBi {
+		public static List<T> Load<T>(string file, bool clear = false) where T : class {
+			var serializator = new BinaryFormatter();
+			var res = new List<T>();
+
+			try {
+				if (!File.Exists(file))
+					return res;
+
+				using (var fs = new FileStream(file, FileMode.Open)) {
+					if (fs.Length == 0) return res;
+
+					do {
+						var o = serializator.Deserialize(fs) as T;
+						if (o != null) res.Add(o);
+					} while (fs.Position < fs.Length);
+
+					if (clear) fs.SetLength(0);
+				}
+			} catch (Exception ex) {
+				// ignored
+			}
+
+			return res;
+		}
+
+		public static bool Save<T>(string file, IEnumerable<T> items, bool append = false) where T : class {
+			var serializator = new BinaryFormatter();
+			CreateIfNotExist(file);
+
+			try {
+				using (var fs = new FileStream(file, append ? FileMode.Append : FileMode.Create))
+					foreach (var item in items)
+						serializator.Serialize(fs, item);
+
+				return true;
+			} catch (Exception ex) {
+				return false;
+			}
+		}
+
+		public static bool Save<T>(string file, T item, bool append = false) where T : class {
+			var serializator = new BinaryFormatter();
+			CreateIfNotExist(file);
+
+			try {
+				using (var fs = new FileStream(file, append ? FileMode.Append : FileMode.Create))
+					serializator.Serialize(fs, item);
+
+				return true;
+			} catch (Exception ex) {
+				return false;
+			}
+		}
+
+		private static void CreateIfNotExist(string file) {
+			try {
+				if (File.Exists(file))
+					return;
+
+				var path = Path.GetDirectoryName(file);
+				if (path.IsBad())
+					return;
+
+				if (!Directory.Exists(path))
+					Directory.CreateDirectory(path);
+			} catch (Exception) {
+				// ignored
+			}
+		}
+	}
+}
---
+++ artzilla.sharp.lib/artzilla.sharp.lib/Serialization/SerXml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using ArtZilla.Sharp.Lib.Extenstions;
+
+namespace ArtZilla.Sharp.Lib.Serialization {
+	public static class SerXml {
+		public static T Load<T>(string file, bool clear = false) where T : class {
+			// Будет исключение, если тип нельзя сериализовать в XML
+			var serializator = new XmlSerializer(typeof(T));
+
+			T res = null;
+
+			try {
+				if (!File.Exists(file))
+					return null;
+
+				using (var fs = new FileStream(file, FileMode.Open)) {
+					if (fs.Length == 0)
+						return null;
+
+					res = serializator.Deserialize(fs) as T;
+
+					if (clear)
+						fs.SetLength(0);
+				}
+			} catch (Exception ex) {
+				// ignored
+			}
+
+			return res;
+		}
+
+		public static bool Save<T>(string file, T item, bool append = false) where T : class {
+			// Будет исключение, если тип нельзя сериализовать в XML
+			var serializator = new XmlSerializer(typeof(T));
+
+			CreateIfNotExist(file);
+
+			try {
+				using (var fs = new FileStream(file, append ? FileMode.Append : FileMode.Create))
+					serializator.Serialize(fs, item);
+
+				return true;
+			} catch (Exception ex) {
+				return false;
+			}
+		}
+
+		private static void CreateIfNotExist(string file) {
+			try {
+				if (File.Exists(file))
+					return;
+
+				var path = Path.GetDirectoryName(file);
+				if (path.IsBad())
+					return;
+
+				if (!Directory.Exists(path))
+					Directory.CreateDirectory(path);
+			} catch (Exception ex) {
+				// ignored
+			}
+		}
+	}
+}
