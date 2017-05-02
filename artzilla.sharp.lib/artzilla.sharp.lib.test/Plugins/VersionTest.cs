3011ce979db421eacd265a4a8fc5ca7894398796
Artyom Fomin <mail@artzilla.name>
2017-05-02 20:13:23 +0600

    micro-fix

--- artzilla.sharp.lib/artzilla.sharp.lib.test/Plugins/VersionTest.cs
+++ artzilla.sharp.lib/artzilla.sharp.lib.test/Plugins/VersionTest.cs
@@ -6,8 +6,8 @@ namespace ArtZilla.Sharp.Lib.Test.Plugins {
 	public class VersionTest {
 		[TestMethod]
 		public void SimpleTest() {
-			var v0 = new VersionInfo(1, 2, 3, 4); 
-			var h0 = new VersionInfo(4, 3, 2, 1); 
+			var v0 = new VersionInfo(1, 2, 3, 4);
+			var h0 = new VersionInfo(4, 3, 2, 1);
 
 			Assert.AreNotEqual(v0, h0);
 			Assert.AreNotEqual(v0.Value(), h0.Value());
@@ -15,11 +15,11 @@ namespace ArtZilla.Sharp.Lib.Test.Plugins {
 			var v1 = new VersionInfo("1.2.3.4");
 			var v2 = new VersionInfo(v0.ToString());
 
-			AssertEx.IsFail(()=> { new VersionInfo(null); });
-			AssertEx.IsFail(()=> { new VersionInfo(".1.1.1"); });
-			AssertEx.IsFail(()=> { new VersionInfo("1.1.1."); });
-			AssertEx.IsFail(()=> { new VersionInfo("1.1.1.1a"); });
-			AssertEx.IsFail(()=> { new VersionInfo("1.1.1.100000000"); });
+			AssertEx.IsFail(() => new VersionInfo(null));
+			AssertEx.IsFail(() => new VersionInfo(".1.1.1"));
+			AssertEx.IsFail(() => new VersionInfo("1.1.1."));
+			AssertEx.IsFail(() => new VersionInfo("1.1.1.1a"));
+			AssertEx.IsFail(() => new VersionInfo("1.1.1.100000000"));
 		}
 	}
 }
