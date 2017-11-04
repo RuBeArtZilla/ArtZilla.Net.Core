37071876d0dacdb8a3b3e67bb3e2114490d8520f
Artyom Fomin <mail@artzilla.name>
2017-11-04 22:04:16 +0600

    [1.1.0.8] some extension methods added

---
+++ artzilla.sharp.lib/artzilla.sharp.lib/Extensions/IDictionaryExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArtZilla.Net.Core.Extensions {
+	public static class IDictionaryExtensions {
+		/// <summary>
+		/// Gets the value associated with the specified key or default value if key not exist
+		/// </summary>
+		/// <typeparam name="TKey">The type of keys in the dictionary.</typeparam>
+		/// <typeparam name="TValue">The type of values in the dictionary.</typeparam>
+		/// <param name="dictionary">Represents a generic collection of key/value pairs.</param>
+		/// <param name="key">The key whose value to get</param>
+		/// <param name="defaultValue">The default value for the type of the value parameter if <paramref name="key"/> not exist</param>
+		/// <returns>Value if <paramref name="dictionary"/> contains an element with the specified <paramref name="key"/>; otherwise <paramref name="defaultValue"/></returns>
+		/// <exception cref="ArgumentNullException"><paramref name="key"/> is null</exception>
+		/// <exception cref="NullReferenceException" ><paramref name="dictionary"/> is null</exception>
+		public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue = default)
+			=> dictionary.TryGetValue(key, out var value) ? value : defaultValue;
+	}
+}
---
+++ artzilla.sharp.lib/artzilla.sharp.lib/Extensions/IListExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArtZilla.Net.Core.Extensions {
+	public static class IListExtensions {
+		/// <summary>
+		/// Adds an <paramref name="item"/> to the <paramref name="list"/> and return it.
+		/// </summary>
+		/// <typeparam name="T">The type of the elements in the code.</typeparam>
+		/// <param name="list">Represents a collection of objects that can be individually accessed by index.</param>
+		/// <param name="item">The object to add to the <paramref name="list"/>.</param>
+		/// <returns>Added <paramref name="item"/></returns>
+		/// <exception cref="ArgumentNullException"><paramref name="list"/> is null</exception>
+		/// <exception cref="NotSupportedException"><paramref name="list"/> is read-only.</exception>
+		public static T AddReturn<T>(this IList<T> list, T item) {
+			Guard.NotNull(list, nameof(list));
+			list.Add(item);
+			return item;
+		}
+	}
+}
