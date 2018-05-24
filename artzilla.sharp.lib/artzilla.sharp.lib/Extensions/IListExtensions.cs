b6fe257a67c8fc574322d030112c09b4f160864b
ArtZilla <mail@artzilla.name>
2018-05-24 14:20:16 +0600

    new version of IList<> extension.

--- artzilla.sharp.lib/artzilla.sharp.lib/Extensions/IListExtensions.cs
+++ artzilla.sharp.lib/artzilla.sharp.lib/Extensions/IListExtensions.cs
@@ -17,5 +17,21 @@ namespace ArtZilla.Net.Core.Extensions {
 			list.Add(item);
 			return item;
 		}
+
+		/// <summary>
+		/// Adds an <paramref name="item"/> to the <paramref name="list"/> and return it.
+		/// </summary>
+		/// <typeparam name="TListItem">List item type</typeparam>
+		/// <typeparam name="TItem">Item type</typeparam>
+		/// <param name="list">Represents a collection of objects that can be individually accessed by index.</param>
+		/// <param name="item">The object to add to the <paramref name="list"/>.</param>
+		/// <returns>Added <paramref name="item"/></returns>
+		/// <exception cref="ArgumentNullException"><paramref name="list"/> is null</exception>
+		/// <exception cref="NotSupportedException"><paramref name="list"/> is read-only.</exception>
+		public static TItem AddReturn<TListItem, TItem>(this IList<TListItem> list, TItem item) where TItem : TListItem {
+			Guard.NotNull(list, nameof(list));
+			list.Add(item);
+			return item;
+		}
 	}
 }
