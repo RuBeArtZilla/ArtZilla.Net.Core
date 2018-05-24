0e6b962ac07625eec44ccb8db7769f48fa914049
ArtZilla <mail@artzilla.name>
2018-05-24 15:17:59 +0600

    removed extension method.

--- artzilla.sharp.lib/artzilla.sharp.lib/Extensions/IListExtensions.cs
+++ artzilla.sharp.lib/artzilla.sharp.lib/Extensions/IListExtensions.cs
@@ -6,21 +6,6 @@ namespace ArtZilla.Net.Core.Extensions {
 		/// <summary>
 		/// Adds an <paramref name="item"/> to the <paramref name="list"/> and return it.
 		/// </summary>
-		/// <typeparam name="T">The type of the elements in the code.</typeparam>
-		/// <param name="list">Represents a collection of objects that can be individually accessed by index.</param>
-		/// <param name="item">The object to add to the <paramref name="list"/>.</param>
-		/// <returns>Added <paramref name="item"/></returns>
-		/// <exception cref="ArgumentNullException"><paramref name="list"/> is null</exception>
-		/// <exception cref="NotSupportedException"><paramref name="list"/> is read-only.</exception>
-		public static T AddReturn<T>(this IList<T> list, T item) {
-			Guard.NotNull(list, nameof(list));
-			list.Add(item);
-			return item;
-		}
-
-		/// <summary>
-		/// Adds an <paramref name="item"/> to the <paramref name="list"/> and return it.
-		/// </summary>
 		/// <typeparam name="TListItem">List item type</typeparam>
 		/// <typeparam name="TItem">Item type</typeparam>
 		/// <param name="list">Represents a collection of objects that can be individually accessed by index.</param>
