fdb3f3ab9a2443af13077e04950cd718228bfddc
ArtZilla <mail@artzilla.name>
2018-09-02 16:11:53 +0600

    PInvoke++

---
+++ artzilla.sharp.lib/artzilla.sharp.lib.test/Serialization/SpecialSerializerTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Xml.Serialization;
+using ArtZilla.Net.Core.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArtZilla.Net.Core.Tests.Serialization {
+	[TestClass]
+	public class SpecialSerializerTests {
+		private interface IWaifu {
+			[XmlAttribute]
+			string Name { get; set; }
+			int Charm { get; set; }
+		}
+
+		private class Waifu : IWaifu {
+			public string Name { get; set; }
+			public int Charm { get; set; }
+		}
+
+		private const string HomuraWaifuText = "<?xml version=\"1.0\"?><Waifu Name=\"Homura\"><Charm>9999</Charm></Waifu>";
+
+		/* not yet ready
+		[TestMethod]
+		public void FromStringTest() {
+			var ser = new SpecialSerializer(typeof(IWaifu), typeof(Waifu));
+			var waifu = ser.Deserialize(HomuraWaifuText) as IWaifu;
+
+			Assert.IsNotNull(waifu, "deserialization from string failed");
+			Assert.AreEqual("Homura", waifu.Name);
+			Assert.AreEqual(9999, waifu.Charm);
+
+			var text = ser.SerializeToString(waifu);
+			Assert.AreEqual(HomuraWaifuText, text);
+		}*/
+	}
+}
--- artzilla.sharp.lib/artzilla.sharp.lib/PInvoke/user32.cs
+++ artzilla.sharp.lib/artzilla.sharp.lib/PInvoke/user32.cs
@@ -2,9 +2,7 @@
 using System.Runtime.InteropServices;
 
 namespace ArtZilla.Net.Core.PInvoke {
-	/// <summary>
-	/// Second argument for function <see cref="User32.ShowWindowAsync"/> 
-	/// </summary>
+	/// <summary> Second argument for function <see cref="User32.ShowWindowAsync"/> </summary>
 	public enum ShowWindowCommand {
 		Hide = 0,
 		Normal = 1,
@@ -15,8 +13,37 @@ namespace ArtZilla.Net.Core.PInvoke {
 		Default = 10,
 	}
 
+	public static class WM {
+		public const int Close = 0x0010;
+		public const int Quit = 0x0012;
+		public const int ShowWindow = 0x0018;
+	}
+
 	public enum PostMessages {
-		WM_CLOSE = 0x0010,
+		WM_CLOSE = WM.Close,
+		WM_QUIT = WM.Quit,
+		WM_SHOWWINDOW = WM.ShowWindow,
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public struct POINT {
+		public int X;
+		public int Y;
+
+		public POINT(int x, int y) {
+			X = x;
+			Y = y;
+		}
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public struct MSG {
+		public IntPtr hWnd;
+		public uint message;
+		public IntPtr wParam;
+		public IntPtr lParam;
+		public uint time;
+		public POINT pt;
 	}
 
 	public static class User32 {
@@ -46,5 +73,77 @@ namespace ArtZilla.Net.Core.PInvoke {
 		public static bool PostMessage(IntPtr hWnd, PostMessages msg, IntPtr wParam, IntPtr lParam)
 			=> PostMessage(hWnd, (uint) msg, wParam, lParam);
 
+
+		/// <summary>
+		/// The GetMessage function retrieves a message from the calling thread's 
+		/// message queue. The function dispatches incoming sent messages until a 
+		/// posted message is available for retrieval. 
+		/// </summary>
+		/// <param name="lpMsg">
+		/// Pointer to an MSG structure that receives message information from 
+		/// the thread's message queue.
+		/// </param>
+		/// <param name="hWnd">
+		/// Handle to the window whose messages are to be retrieved.
+		/// </param>
+		/// <param name="wMsgFilterMin">
+		/// Specifies the integer value of the lowest message value to be 
+		/// retrieved. 
+		/// </param>
+		/// <param name="wMsgFilterMax">
+		/// Specifies the integer value of the highest message value to be 
+		/// retrieved.
+		/// </param>
+		/// <returns></returns>
+		[DllImport(DllName)]
+		internal static extern bool GetMessage(
+			out MSG lpMsg,
+			IntPtr hWnd,
+			uint wMsgFilterMin,
+			uint wMsgFilterMax);
+
+		/// <summary>
+		/// The TranslateMessage function translates virtual-key messages into 
+		/// character messages. The character messages are posted to the calling 
+		/// thread's message queue, to be read the next time the thread calls the 
+		/// GetMessage or PeekMessage function.
+		/// </summary>
+		/// <param name="lpMsg"></param>
+		/// <returns></returns>
+		[DllImport(DllName)]
+		internal static extern bool TranslateMessage([In] ref MSG lpMsg);
+
+		/// <summary>
+		/// The DispatchMessage function dispatches a message to a window 
+		/// procedure. It is typically used to dispatch a message retrieved by 
+		/// the GetMessage function.
+		/// </summary>
+		/// <param name="lpMsg"></param>
+		/// <returns></returns>
+		[DllImport(DllName)]
+		internal static extern IntPtr DispatchMessage([In] ref MSG lpMsg);
+
+		/// <summary>
+		/// The PostThreadMessage function posts a message to the message queue 
+		/// of the specified thread. It returns without waiting for the thread to 
+		/// process the message.
+		/// </summary>
+		/// <param name="idThread">
+		/// Identifier of the thread to which the message is to be posted.
+		/// </param>
+		/// <param name="msg">Specifies the type of message to be posted.</param>
+		/// <param name="wParam">
+		/// Specifies additional message-specific information.
+		/// </param>
+		/// <param name="lParam">
+		/// Specifies additional message-specific information.
+		/// </param>
+		/// <returns></returns>
+		[DllImport(DllName)]
+		internal static extern bool PostThreadMessage(
+			uint idThread,
+			uint msg,
+			UIntPtr wParam,
+			IntPtr lParam);
 	}
 }
