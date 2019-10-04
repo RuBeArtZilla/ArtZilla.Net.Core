ec9d50aed8e67fba01226c5132bf5bdacf38705b
ArtZilla <mail@artzilla.name>
2019-10-04 11:44:19 +0600

    [1.1.5] add IsNullOrEmpty and IsNotNullOrEmpty array extension methods

---
+++ artzilla.sharp.lib/artzilla.sharp.lib.test/Extensions/ArrayExtTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using ArtZilla.Net.Core.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArtZilla.Net.Core.Tests.Extensions {
+  [TestClass]
+  [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+  public class ArrayExtTests {
+	  public static readonly int[] NullArray = default;
+	  public static readonly int[] EmptyArray = new int[0];
+	  public static readonly int[] NotEmptyArray = { 4, 8, 15, 16, 23, 42 };
+
+		[TestMethod]
+		public void IsNullOrEmptyTest() {
+			Assert.IsTrue(NullArray.IsNullOrEmpty());
+			Assert.IsTrue(EmptyArray.IsNullOrEmpty());
+			Assert.IsFalse(NotEmptyArray.IsNullOrEmpty());
+		}
+
+		[TestMethod]
+		public void IsNotNullOrEmptyTest() {
+			Assert.IsFalse(NullArray.IsNotNullOrEmpty());
+			Assert.IsFalse(EmptyArray.IsNotNullOrEmpty());
+			Assert.IsTrue(NotEmptyArray.IsNotNullOrEmpty());
+		}
+	}
+}
