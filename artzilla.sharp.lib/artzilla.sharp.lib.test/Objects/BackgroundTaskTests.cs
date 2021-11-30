8c5708b625dc1d4a6a4d5b3c569597e22645ad43
ArtZilla <mail@artzilla.name>
2021-11-30 17:43:07 +0600

    work on background task

---
+++ artzilla.sharp.lib/artzilla.sharp.lib.test/Objects/BackgroundTaskTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArtZilla.Net.Core.Tests.Objects {
+	[TestClass]
+	public class BackgroundTaskTests {
+
+		[TestMethod]
+		public void Example() {
+			var local = 0;
+
+			async Task LocalTask(int a, CancellationToken token = default) {
+				local = -a;
+				await Task.Delay(10, token);
+				local = a;
+				Console.WriteLine(a);
+			}
+
+			var task = new BackgroundTask<int>(LocalTask);
+			for (var i = 0; i <= 10; i++) 
+				task.Run(i);
+
+			var completed = SpinWait.SpinUntil(() => !task.IsExecuting, 1000);
+			Assert.IsTrue(completed);
+
+			Assert.AreEqual(10, local);
+		}
+
+		[TestMethod]
+		public async Task CancelExampleAsync() {
+			async Task LocalTask(int a, CancellationToken token) 
+				=> await Task.Delay(TimeSpan.FromSeconds(2D), token);
+
+			var sw = Stopwatch.StartNew();
+			var task = new BackgroundTask<int>(LocalTask);
+			
+			task.Run(0);
+			Assert.IsTrue(task.IsExecuting);
+			
+			task.Cancel();
+			Assert.IsFalse(task.IsExecuting);
+
+			await task.RunAsync(0);
+			Assert.IsTrue(task.IsExecuting);
+
+			task.Cancel();
+			Assert.IsFalse(task.IsExecuting);
+
+			Assert.IsFalse(sw.Elapsed.Seconds > 2D);
+		}
+
+	}
+}
---
+++ artzilla.sharp.lib/artzilla.sharp.lib/BkgOp/BackgroundTask.cs
@@ -0,0 +1,76 @@
+﻿#if !NET40
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ArtZilla.Net.Core {
+	/// task repeated on background thread
+	public sealed class BackgroundTask<T> {
+		/// todo: ...
+		public delegate Task BackgroundAction(T arg, CancellationToken token = default);
+
+		/// todo: ...
+		public delegate void OnError(T arg, Exception exception);
+		
+		/// todo: ...
+		public bool IsExecuting { get; private set; }
+
+		/// todo: ...
+		public BackgroundTask(BackgroundAction action) 
+			: this(action, OnErrorDefault) { }
+
+		/// todo: ...
+		public BackgroundTask(BackgroundAction action, OnError onError) {
+			_action = action;
+			_onError = onError ?? throw new ArgumentNullException(nameof(onError));
+		}
+
+		/// todo: ...
+		public void Cancel() 
+			=> _cts.Cancel();
+		
+		/// todo: ...
+		public void Run(T arg) 
+			=> ExecuteAsync(arg);
+
+		/// todo: ...
+		public async Task RunAsync(T arg) 
+			=> ExecuteAsync(arg);
+
+		private async Task ExecuteAsync(T arg) {
+			_args.Enqueue(arg);
+
+			Task.Run(() => ExecuteAsync()); // await ExecuteAsync();
+		}
+
+		private async Task ExecuteAsync() {
+			IsExecuting = true;
+
+			var token = _cts.Token;
+			while (!token.IsCancellationRequested && _args.Count > 0) {
+				var arg = _args.Dequeue();
+
+				try {
+					await _action(arg, token);
+				} catch (Exception e) {
+					_onError(arg, e);
+				}
+			}
+
+			IsExecuting = false;
+		}
+
+		private static void OnErrorDefault(T arg, Exception exception) 
+			=> Console.WriteLine($"BackgroundTask({arg}) thrown {exception.Message}");
+
+		private readonly OnError _onError;
+		private readonly BackgroundAction _action;
+		private readonly Queue<T> _args = new ();
+		private readonly CancellationTokenSource _cts = new ();
+	}
+}
+
+#endif
